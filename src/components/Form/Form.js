7446c23b6c100f2acb08da09974090b425beba0b
OlgaSavytska <o.savytska77@gmail.com>
2019-10-10 00:51:13 +0300

    10

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,71 @@
-import React from 'react';
-// import s from './Form.module.css';
+import React, { Component } from 'react';
+import s from './Form.module.css';
 
-const Form = () => {
-  return <p>Hello</p>
+class Form extends Component {
+  state = {
+    inputAgree: true,
+    inputData: '',
+    inputDay: '',
+    inputNumber: '',
+    inputAdress: '',
+    inputYesOrNot: false,
+    inputOkNoProblem: true
+  }
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // const { inputAgree,
+    //   inputData,
+    //   inputDay,
+    //   inputNumber,
+    //   inputAdress,
+    //   inputYesOrNot,
+    //   inputOkNoProblem } = this.state;
+
+    this.setState({
+      inputAgree: true,
+      inputData: '',
+      inputDay: '',
+      inputNumber: '',
+      inputAdress: '',
+      inputYesOrNot: false,
+      inputOkNoProblem: true
+    });
+  };
+  handleChecked = () => {
+    this.setState({ inputAgree: !this.state.inputAgree })
+    console.log(this.state);
+  }
+
+
+  render() {
+    const {
+      inputData,
+      inputDay,
+      inputNumber,
+      inputAdress,
+    } = this.state;
+    return (<div className={s.delivery_container} >
+      <div className={s.form_cont}>
+        <h2 className={s.form_title}>Бланк на доставку виграшу</h2>
+        <form className={s.form} onSubmit={this.handleChecked}>
+          <input
+            name="inputAgree"
+            checked={this.state.inputAgree}
+            type="checkbox"
+            onChange={this.handleChecked}
+            className={s.form_agree} />
+        </form>
+      </div>
+    </div >)
+  }
 }
 
 export default Form;
