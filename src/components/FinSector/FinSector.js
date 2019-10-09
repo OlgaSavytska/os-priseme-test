a45faaf9098f79e8d0805eb7b18391882a22ccae
OlgaSavytska <o.savytska77@gmail.com>
2019-10-09 13:49:28 +0300

    5

--- src/components/FinSector/FinSector.js
+++ src/components/FinSector/FinSector.js
@@ -9,6 +9,8 @@ const FinSector = () => {
     <p className={s.fin_sector_name1}>вітаємо!</p>
     <p className={s.fin_sector_congratulations}>Ви успішно пройшли всі етапи відбору і отримуєте</p>
     <p className={s.fin_sector_red_title}>ЧЕРВОНИЙ ДИПЛОМ ПЕРЕМОЖЦЯ!</p>
+    <p className={s.fin_sector_proud_of_you}>Фінансовий відділ “PrizeMe” пишається Вами!</p>
+    <p className={s.fin_sector_p_f_y_descr1}>Ви дуже швидко реагували на листи і правильно заповнювали офіційні документи, саме тому перед нагородженням ми вирішили видати Вам ЧЕРВОНИЙ ДИПЛОМ ПЕРЕМОЖЦЯ, що надає Вам унікальне право на першочергову та БЕЗКОШТОВНУ доставку виграшу.</p>
   </div>
 }
 
