853d15b0f6ddd5c8d593af3e88cff4e6147a9de8
OlgaSavytska <o.savytska77@gmail.com>
2019-10-11 12:05:24 +0300

    41

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,27 +9,31 @@ const Header = () => {
   return <div className={s.container_header}>
     <div className={s.header_color1}>
       {/* <img src={back} alt="Elipse" className={s.elipse} /> */}
-      <div className={s.borders}>
-        <div className={s.text}>
-          <p className={s.text_little_header}>*У разі Вашої перемоги в Акції "Золотий Фонд" та виникнення подібної ситуації Ви можете отримати такого листа */</p>
-          <img src={logo} alt="Logo" />
-          <p className={s.name_user}>Name</p>
-          <p className={s.text_prise}>На вас чекає грандіозне нагородження!</p>
-          <p className={s.header_text_description}>Від щирого серця вітаю Вас! Ви заслужено здобули перемогу і будете винагороджені по максимуму! Мені приємно усвідомлювати, що саме Ви отримаєте Головний приз Акції – 317 000,00 гривень!</p>
-          <Link
-            activeClass="active"
-            to="Fr"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={1500}
-            className={s.header_button}>
-            <p className={s.header_button_text}>обрати дату {'\n'}
-              нагородження</p>
-          </Link>
+      <div className={s.norm}>
+        <div className={s.borders}>
+
+          <div className={s.text}>
+            <p className={s.text_little_header}>*У разі Вашої перемоги в Акції "Золотий Фонд" та виникнення подібної ситуації Ви можете отримати такого листа */</p>
+            <img src={logo} alt="Logo" />
+            <p className={s.name_user}>Name</p>
+            <p className={s.text_prise}>На вас чекає грандіозне нагородження!</p>
+            <p className={s.header_text_description}>Від щирого серця вітаю Вас! Ви заслужено здобули перемогу і будете винагороджені по максимуму! Мені приємно усвідомлювати, що саме Ви отримаєте Головний приз Акції – 317 000,00 гривень!</p>
+            <Link
+              activeClass="active"
+              to="Fr"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1500}
+              className={s.header_button}>
+              <p className={s.header_button_text}>обрати дату {'\n'}
+                нагородження</p>
+            </Link>
+          </div>
+          <div className={s.dit}></div>
+          <img src={woman} alt="happy woman" className={s.header_woman} />
+          <p className={s.block_signature}>З повагою, Голова Фінансового відділу {'\n'}з виплати призів Олена Філімонова. *</p>
         </div>
-        <img src={woman} alt="happy woman" className={s.header_woman} />
-        <p className={s.block_signature}>З повагою, Голова Фінансового відділу {'\n'}з виплати призів Олена Філімонова. *</p>
       </div>
     </div>
   </div >
--- src/components/UAreTheBest/UAreTheBest.js
+++ src/components/UAreTheBest/UAreTheBest.js
@@ -8,11 +8,13 @@ const UAreTheBest = () => {
     <div className={s.best_color1}>
       {/* <img src={back} alt="Elipse" className={s.elipse1} /> */}
       <div className={s.borders2}>
-        <p className={s.best_name}>Name,</p>
-        <p className={s.best_descr}>Ви - найкращий клієнт компанії!</p>
-        <p className={s.best_descr2}>Ви - Власник ЧЕРВОГО ДИПЛОМА ПЕРЕМОЖЦЯ!</p>
-        <p className={s.best_descr3}>Ми вже готові доставити Вам виграш за адресою Вашого проживання. Головне – повідомте, коли Вам буде зручно зустріти кур’єра компанії та одержати гроші.</p>
-        <p className={s.best_descr4}>Ви можете обрати будь-який тиждень виплат та день (навіть вихідний) для візиту кур’єра.</p>
+        <div className={s.div_flex}>
+          <p className={s.best_name}>Name,</p>
+          <p className={s.best_descr}>Ви - найкращий клієнт компанії!</p>
+          <p className={s.best_descr2}>Ви - Власник ЧЕРВОГО ДИПЛОМА ПЕРЕМОЖЦЯ!</p>
+          <p className={s.best_descr3}>Ми вже готові доставити Вам виграш за адресою Вашого проживання. Головне – повідомте, коли Вам буде зручно зустріти кур’єра компанії та одержати гроші.</p>
+          <p className={s.best_descr4}>Ви можете обрати будь-який тиждень виплат та день (навіть вихідний) для візиту кур’єра.</p>
+        </div>
         <img src={man} alt="man" className={s.man}></img>
       </div>
     </div>
