380e9c3cdf68fed45f6aba9168b42fc3364200bc
OlgaSavytska <o.savytska77@gmail.com>
2019-10-11 10:38:29 +0300

    30

--- src/components/Delivery/Delivery.js
+++ src/components/Delivery/Delivery.js
@@ -6,9 +6,11 @@ import copyy from '../../images/copy.png';
 const Delivery = () => {
   return <div className={s.delivery_container}>
     <p className={s.delivery_title}>Ми надсилаємо Вам “БЛАНК НА ДОСТАВКУ ВИГРАШУ”.</p>
-    <h2 className={s.delivery_name}>Name</h2>
     <div className={s.div_flex}>
-      <p className={s.delivery_istruction}>щоб отримати свій виграш – впишіть у “БЛАНК” зручну для Вас дату отримання виграшу та надішліть нам. Після цього кур’єр “PrizeMe” доставить Вам {'\n'}<span className={s.num}>317 000,00 гривень</span> прямо додому.</p>
+      <div className={s.div_flex2}>
+        <h2 className={s.delivery_name}>Name</h2>
+        <p className={s.delivery_istruction}>щоб отримати свій виграш – впишіть у “БЛАНК” зручну для Вас дату отримання виграшу та надішліть нам. Після цього кур’єр “PrizeMe” доставить Вам {'\n'}<span className={s.num}>317 000,00 гривень</span> прямо додому.</p>
+      </div>
       <img className={s.copy} src={copyy} alt="copy" />
     </div>
     <p className={s.delivery_istruction_border}>ЗАПОВНЕНИЙ “БЛАНК НА ДОСТАВКУ ВИГРАШУ” – ВАША ГАРАНТІЯ ОТРИМАННЯ <span className={s.num}>317 000,00 ГРИВЕНЬ!</span></p>
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -54,11 +54,6 @@ class Form extends Component {
     this.setState({ nputOkNoProblem: !this.state.nputOkNoProblem })
     console.log(this.state);
   }
-  // handleDataChecked = (inputData1) => {
-  //   const inputData1 = "hhhhhhh";
-  //   this.setState({ inputData: this.state.inputData1 })
-  //   console.log(this.state);
-  // }
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -80,152 +75,154 @@ class Form extends Component {
 
 
     return (<div className={s.delivery_container} >
-      <form className={s.form} onSubmit={this.handleChecked}>
-        <div className={s.form_cont}>
-          <h2 id="Fr" className={s.form_title}>Бланк на доставку виграшу</h2>
-          <div className={s.form_flex}>
-
-            <input
-              name="inputAgree"
-              checked={this.state.inputAgree}
-              type="checkbox"
-              onChange={this.handleChecked}
-              className={s.form_agree} />
-            <label className={s.label1}><p className={s.label1_text
-            }>Я, Name,{'\n'} як Головний переможець Акції погоджуюсь отримати свій виграш – 317 000,00 гривень – готівкою.</p></label>
-          </div>
-          <p className={s.label2_text}>Я очікую доставку грошового Призу:</p>
-          <div className={s.flex_div4}>
-            <div className={s.data_block}>
+      <div className={s.div_flex}>
+        <form className={s.form} onSubmit={this.handleChecked}>
+          <div className={s.form_cont}>
+            <h2 id="Fr" className={s.form_title}>Бланк на доставку виграшу</h2>
+            <div className={s.form_flex}>
               <input
-                name="inputData"
-                value={'Першого тижня виплат (01–05.05)'}
-                type="radio"
-                onChange={this.handleChange}
-                className={s.form_data} />
-              <label className={s.label2}>
-                <p className={s.label3_text}>Першого тижня виплат (01–05.05)</p>
-              </label>
+                name="inputAgree"
+                checked={this.state.inputAgree}
+                type="checkbox"
+                onChange={this.handleChecked}
+                className={s.form_agree} />
+              <label className={s.label1}><p className={s.label1_text
+              }>Я, Name,{'\n'} як Головний переможець Акції погоджуюсь отримати свій виграш – 317 000,00 гривень – готівкою.</p></label>
             </div>
-            <div className={s.data_block}>
+            <p className={s.label2_text}>Я очікую доставку грошового Призу:</p>
+            <div className={s.flex_div4}>
+              <div className={s.data_block}>
+                <input
+                  name="inputData"
+                  value={'Першого тижня виплат (01–05.05)'}
+                  type="radio"
+                  onChange={this.handleChange}
+                  className={s.form_data} />
+                <label className={s.label2}>
+                  <p className={s.label3_text}>Першого тижня виплат (01–05.05)</p>
+                </label>
+              </div>
+              <div className={s.data_block}>
+                <input
+                  name="inputData"
+                  value={'Другого тижня виплат (06–12.05)'}
+                  type="radio"
+                  onChange={this.handleChange}
+                  className={s.form_data} />
+                <label className={s.label2}>
+                  <p className={s.label3_text}>Другого тижня виплат (06–12.05)</p>
+                </label>
+              </div>
+              <div className={s.data_block}>
+                <input
+                  name="inputData"
+                  value={'Третього тижня виплат (13–19.05)'}
+                  type="radio"
+                  onChange={this.handleChange}
+                  className={s.form_data} />
+                <label className={s.label2}>
+                  <p className={s.label3_text}>Третього тижня виплат (13–19.05)</p>
+                </label>
+              </div>
+              <div className={s.data_block}>
+                <input
+                  name="inputData"
+                  value={'Четвертого тижня виплат (20–26.05)'}
+                  type="radio"
+                  onChange={this.handleChange}
+                  className={s.form_data} />
+                <label className={s.label2}>
+                  <p className={s.label3_text}>Четвертого тижня виплат (20–26.05)</p>
+                </label>
+              </div>
+              <div className={s.data_block}>
+                <input
+                  name="inputData"
+                  value={'П’ятого тижня виплат (27.05–31.05).'}
+                  type="radio"
+                  onChange={this.handleChange}
+                  className={s.form_data} />
+                <label className={s.label2}>
+                  <p className={s.label3_text}>П’ятого тижня виплат (27.05–31.05).</p>
+                </label>
+              </div>
+            </div>
+            <div className={s.div_flex5}>
+              <div className={s.input_options_section}>
+                <p className={s.select_desct}>В зручний для мене день тижня:</p>
+                <Select
+                  required
+                  className={s.inputDay}
+                  value={findOption(inputDay)}
+                  options={options}
+                  onChange={this.onChangeSelect}
+                >
+                </Select>
+              </div>
+              <div className={s.input_number}>
+                <p className={s.select_desct2}>Номер телефону для зв’язку:</p>
+                <input
+                  name="inputNumber"
+                  type="number"
+                  value={inputNumber}
+                  onChange={this.handleChange}
+                  className={s.inputNumber}
+                  placeholder="+380(050)555-555-5"
+                  required
+                />
+              </div>
+            </div>
+            <div className={s.input_adress}>
+              <p className={s.select_desct3}>Номер телефону для зв’язку:</p>
               <input
-                name="inputData"
-                value={'Другого тижня виплат (06–12.05)'}
-                type="radio"
+                maxLength="1000"
+                minLength="3"
+                name="inputAdress"
+                type="text"
+                className={s.input_adress_form}
+                placeholder="Область/Район/Населений пункт/Вулиця/Номер будинку/Номер квартири"
+                value={inputAdress}
                 onChange={this.handleChange}
-                className={s.form_data} />
-              <label className={s.label2}>
-                <p className={s.label3_text}>Другого тижня виплат (06–12.05)</p>
-              </label>
-            </div>
-            <div className={s.data_block}>
-              <input
-                name="inputData"
-                value={'Третього тижня виплат (13–19.05)'}
-                type="radio"
-                onChange={this.handleChange}
-                className={s.form_data} />
-              <label className={s.label2}>
-                <p className={s.label3_text}>Третього тижня виплат (13–19.05)</p>
-              </label>
-            </div>
-            <div className={s.data_block}>
-              <input
-                name="inputData"
-                value={'Четвертого тижня виплат (20–26.05)'}
-                type="radio"
-                onChange={this.handleChange}
-                className={s.form_data} />
-              <label className={s.label2}>
-                <p className={s.label3_text}>Четвертого тижня виплат (20–26.05)</p>
-              </label>
-            </div>
-            <div className={s.data_block}>
-              <input
-                name="inputData"
-                value={'П’ятого тижня виплат (27.05–31.05).'}
-                type="radio"
-                onChange={this.handleChange}
-                className={s.form_data} />
-              <label className={s.label2}>
-                <p className={s.label3_text}>П’ятого тижня виплат (27.05–31.05).</p>
-              </label>
-            </div>
-          </div>
-          <div className={s.div_flex5}>
-            <div className={s.input_options_section}>
-              <p className={s.select_desct}>В зручний для мене день тижня:</p>
-              <Select
-                required
-                className={s.inputDay}
-                value={findOption(inputDay)}
-                options={options}
-                onChange={this.onChangeSelect}
-              >
-              </Select>
-            </div>
-            <div className={s.input_number}>
-              <p className={s.select_desct2}>Номер телефону для зв’язку:</p>
-              <input
-                name="inputNumber"
-                type="number"
-                value={inputNumber}
-                onChange={this.handleChange}
-                className={s.inputNumber}
-                placeholder="+380(050)555-555-5"
                 required
               />
             </div>
+            <div className={s.final}>
+              <input
+                name="inputYesOrNot"
+                checked={this.state.inputYesOrNot}
+                type="checkbox"
+                onChange={this.handleChecked1}
+                className={s.form_agree1} />
+              <label className={s.label3}><p className={s.label3_text
+              }> Я відмовляюсь від БЕЗКОШТОВНОЇ доставки мені виграшу. Прошу вислати мені 317 000,00 гривень поштовим переказом.</p></label>
+            </div>
+            <div className={s.final}>
+              <input
+                name="inputOkNoProblem"
+                checked={this.state.inputOkNoProblem}
+                type="checkbox"
+                onChange={this.handleChecked2}
+                className={s.form_agree2} />
+              <label className={s.label4}><p className={s.label4_text
+              }> Я заповнюю цей документ і роблю обов’язкове замовлення товарів</p></label>
+            </div>
           </div>
-          <div className={s.input_adress}>
-            <p className={s.select_desct3}>Номер телефону для зв’язку:</p>
-            <input
-              maxLength="1000"
-              minLength="3"
-              name="inputAdress"
-              type="text"
-              className={s.input_adress_form}
-              placeholder="Область/Район/Населений пункт/Вулиця/Номер будинку/Номер квартири"
-              value={inputAdress}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className={s.final}>
-            <input
-              name="inputYesOrNot"
-              checked={this.state.inputYesOrNot}
-              type="checkbox"
-              onChange={this.handleChecked1}
-              className={s.form_agree1} />
-            <label className={s.label3}><p className={s.label3_text
-            }> Я відмовляюсь від БЕЗКОШТОВНОЇ доставки мені виграшу. Прошу вислати мені 317 000,00 гривень поштовим переказом.</p></label>
-          </div>
-          <div className={s.final}>
-            <input
-              name="inputOkNoProblem"
-              checked={this.state.inputOkNoProblem}
-              type="checkbox"
-              onChange={this.handleChecked2}
-              className={s.form_agree2} />
-            <label className={s.label4}><p className={s.label4_text
-            }> Я заповнюю цей документ і роблю обов’язкове замовлення товарів</p></label>
-          </div>
-        </div>
-        <Link
-          activeClass="active"
-          to="Congrat"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={1500}
-          className={s.header_button}>
-          <button type="submit" className={s.button}>
-            НАДІСЛАТИ
+          <Link
+            activeClass="active"
+            to="Congrat"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={1500}
+            className={s.header_button}>
+            <button type="submit" className={s.button}>
+              НАДІСЛАТИ
         </button>
-        </Link >
+          </Link >
+        </form>
         <p className={s.congliration}>Щоб Служба автоматичної верифікації не відбракувала Ваш БЛАНК НА ДОСТАВКУ ВИГРАШУ, зробіть обов’язкове замовлення товарів із акційної пропозиції.</p>
-      </form>
+
+      </div >
     </div >
     )
   }
