6129f1e455d08e687ad70e8bad6fe7f37b661eb1
OlgaSavytska <o.savytska77@gmail.com>
2019-10-11 10:50:05 +0300

    Updates

---
+++ precache-manifest.d1e5fe0caa1c10cfa4e35c181c4753c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b365b36e3d1e33c53ce55ed13b3c30",
+    "url": "/os-priseme-test/build/index.html"
+  },
+  {
+    "revision": "3f9cdcd02a42ad79f801",
+    "url": "/os-priseme-test/build/static/css/main.623dc02f.chunk.css"
+  },
+  {
+    "revision": "847c952c7c75bbd16fbf",
+    "url": "/os-priseme-test/build/static/js/2.c7f29897.chunk.js"
+  },
+  {
+    "revision": "3f9cdcd02a42ad79f801",
+    "url": "/os-priseme-test/build/static/js/main.2a89822d.chunk.js"
+  },
+  {
+    "revision": "b5a1c4fe026f05c5e94d",
+    "url": "/os-priseme-test/build/static/js/runtime-main.70afae2a.js"
+  },
+  {
+    "revision": "85c0cc5aa335b1693d8572e1312e5f59",
+    "url": "/os-priseme-test/build/static/media/Depositphotos_27353059_xl-2015.85c0cc5a.png"
+  },
+  {
+    "revision": "61213aa3b47d27508b79d7ced853691e",
+    "url": "/os-priseme-test/build/static/media/Diplom3.61213aa3.jpg"
+  },
+  {
+    "revision": "dea2d00c8911f5c7a2ba9b0870046b6a",
+    "url": "/os-priseme-test/build/static/media/Diplom4.dea2d00c.jpg"
+  },
+  {
+    "revision": "e7799fe7031e29233e8040eea7545942",
+    "url": "/os-priseme-test/build/static/media/Ellipse.e7799fe7.png"
+  },
+  {
+    "revision": "7b535a1038450b1c6e6456cdffec315b",
+    "url": "/os-priseme-test/build/static/media/Frame5.2.7b535a10.png"
+  },
+  {
+    "revision": "13cfbeada93f783307fa6faa87d30a62",
+    "url": "/os-priseme-test/build/static/media/Frame6.13cfbead.jpg"
+  },
+  {
+    "revision": "e73df6b731ed34fb636a5dda9a899464",
+    "url": "/os-priseme-test/build/static/media/Frame6.e73df6b7.png"
+  },
+  {
+    "revision": "961f9abb77b056aec55342370b49b8d0",
+    "url": "/os-priseme-test/build/static/media/copy.961f9abb.png"
+  },
+  {
+    "revision": "deb9db3083ba98bb476ce1b0ac9cc5ec",
+    "url": "/os-priseme-test/build/static/media/man.deb9db30.png"
+  }
+]);
